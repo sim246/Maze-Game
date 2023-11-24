570474a0bc92d5e0fb5daffc638c118d5e3cc556
Simona Georgiana Dragomir <simona.dragomir@dawsoncollege.qc.ca>
2023-11-24 11:16:00 -0500

    console user interface

--- BenchMarking/Program.cs
+++ BenchMarking/Program.cs
@@ -9,7 +9,56 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        WriteToFile("C:\\Users\\2133009\\Downloads\\dragomirassignment5\\testdoc.txt", "text");
+        bool notvalid = true;
+        int type = 0;
+        int height = 0;
+        int width = 0;
+        Action bench;
+
+        while (notvalid)
+        {
+            try
+            {
+                Console.WriteLine("Input size of maze: ");
+                Console.WriteLine("Input height: ");
+                height = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Input width: ");
+                width = Int32.Parse(Console.ReadLine());
+                
+                if (height > 1 && width > 1)
+                {
+                    notvalid = false;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Maze size invalid!");
+            }
+        }
+        notvalid = true;
+        while (notvalid)
+        {
+            try
+            {
+                Console.WriteLine("Input type of maze: ");
+                Console.WriteLine("MazeHuntKill maze - input 1");
+                Console.WriteLine("MazeRecursion maze - input 2");
+                type = Int32.Parse(Console.ReadLine());
+                if (type == 1)
+                {
+                    notvalid = false;
+
+                }
+                else if (type == 2)
+                {
+                    notvalid = false;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Type of maze invalid!");
+            }
+        }
     }
 
     public static TimeSpan TimeIt(Action bench)
